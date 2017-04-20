d65460eb130dc35b0198221e8ba1098bc1654bce
pingxingkongjian <153248967@qq.com>
2017-04-20 18:46:20 +0800

    Initial commit

---
+++ js/5.js
@@ -0,0 +1,7 @@
+/**
+//  * Created by HP on 2017/4/14.
+//  */
+var box = document.getElementById('box');
+
+box.style.color = "red";
+box.style.background = 'blue';
---
+++ js/text.js
@@ -0,0 +1,26 @@
+/**
+ * Created by HP on 2017/4/14.
+ */
+// var btn = document.getElementById("btn");
+// var number = document.getElementById("number");
+// var addBtn = document.getElementById("addBtn");
+// var unit_price = document.getElementById("unit_price").innerHTML;
+// var total_price = document.getElementById("total_price");
+//
+//
+// addBtn.onclick = function () {
+//     var num =++number.value;
+//     total_price.innerHTML = (num * unit_price).toFixed(2)
+//
+// };
+//
+// btn.onclick = function () {
+//     var num1 = number.value;
+// //        console.log(num1);
+//     if ( num1 <=1 ) {
+//
+//     } else {
+//         var num2 = --number.value;
+//         total_price.innerHTML = (num2 * unit_price).toFixed(2)
+//     }
+// }
